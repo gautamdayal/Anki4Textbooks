2df71d0f3b890f0d1e6e162a3f804fd3514e9c34
Vishnu Nittoor <nitvishn@gmail.com>
2021-06-12 12:13:51 +0530

    score and stuff

--- numberExtraction.py
+++ numberExtraction.py
@@ -3,14 +3,29 @@ s = time.time()
 import networkx as nx
 import pandas as pd
 import cv2
+import numpy as np
+from numpy.linalg import norm
 import re
 import pytesseract
 import matplotlib.pyplot as plt
+import itertools
+import sklearn.metrics
 print(f"Imports completed in {time.time() - s}s")
+
+"""
+How to detect question numbers:
+1. Although some numbers are missed out, they're in sequence most of the time.
+2. They are at the center of the distribution of all the numbers, very dense.
+3. If you draw a line of best fit through them, they're all very close to that line.
+4. That line of best fit shoult point straight downwards.
+"""
 
 def load(somethinglol):
     img = cv2.imread(somethinglol)
+    h, w, c = img.shape
+    print(h, w)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (1000, 1000))
     return img
 
 def select_numbers(img):
@@ -20,31 +35,89 @@ def select_numbers(img):
     - The displacement for each point from the best straight line drawn through the set of points
     - The angle the directed line makes with the x axis
     """
-    def score(numset):
-        return 0
-
     def generateNumsets(G):
         """
-        DFS-based enumeration of each possible numset
+        [Not Anymore] DFS-based enumeration of each possible numset
+        This is just brute force right now, will find faster way to do it later.
+        """
+        # paths = []
+        #
+        # path = [0]
+        # for edge in nx.dfs_edges(G, 0):
+        #     if edge[0] == path[-1]:
+        #         path.append(edge[1])
+        #     else:
+        #         paths.append(path)
+        #         search_index = 2
+        #         while search_index <= len(path):
+        #             if edge[0] == path[-search_index]:
+        #                 path = path[:-search_index + 1] + [edge[1]]
+        #                 break
+        #             search_index += 1
+        #         else:
+        #             raise Exception("Wrong path structure?", path, edge)
+        # paths.append(path)
+        # return paths
+
+        """
+        Trying to use itertools LMAO
+        """
+        # paths = []
+        #
+        # for path in itertools.combinations(G.nodes, 5):
+        #     paths.append(path)
+        # return paths
+
+        """
+        Generating paths using graph
         """
         paths = []
-
-        path = [0]
-        for edge in nx.dfs_edges(G, 0):
-            if edge[0] == path[-1]:
-                path.append(edge[1])
-            else:
-                paths.append(path)
-                search_index = 2
-                while search_index <= len(path):
-                    if edge[0] == path[-search_index]:
-                        path = path[:-search_index + 1] + [edge[1]]
-                        break
-                    search_index += 1
-                else:
-                    raise Exception("Wrong path structure?", path, edge)
-        paths.append(path)
+        n = len(G.nodes)
+        for source in range(n):
+            for target in range(source+1, n):
+                paths.extend([path for path in nx.all_simple_paths(G, source=0, target=16)])
         return paths
+
+        # return paths
+
+    def score(numset):
+        # print(numset, numbers)
+        tot = 0
+        n = len(numset)
+        # consecutivity
+        consecCounter = 0
+        for i in range(n-1):
+            n1 = numset[i]; n2 = numset[i+1]
+            consecCounter += numbers[n2][0] - numbers[n1][0]
+        consecCounter/=n
+
+        tot += abs(1 - consecCounter)
+        # print(tot)
+
+        # check if it's within line of best fit
+
+        X = [numbers[i][1] for i in numset]
+        Y = [numbers[i][2] for i in numset]
+        coeffs = np.polyfit(Y, X, 1)
+        Xfit = np.polyval(coeffs, Y)
+
+        p1 = np.array([Xfit[0], Y[0]])
+        p2 = np.array([Xfit[-1], Y[-1]])
+        totdists = 0
+        for i in range(n):
+            p3 = np.array([X[i], Y[i]])
+            dist = np.abs(np.cross(p2-p1, p1-p3)) / norm(p2-p1)
+            totdists += dist
+
+        tot += totdists/n
+        # plt.scatter(X, Y)
+        # plt.plot(Xfit, Y)
+        # plt.show()
+
+        print(f"Numset: {numset}; Actual numbers: {[numbers[i][0] for i in numset]}; Score: {tot}")
+
+        # see where that line is pointing
+        return tot
 
     def treeify(numbers):
         n = len(numbers)
@@ -60,6 +133,9 @@ def select_numbers(img):
     def unpunctuate(S):
         return re.sub(r'[^\w\s]', '', S)
 
+    def numbersFromNumset(numset):
+        return [numbers[x] for x in numset]
+
     s = time.time()
     words = pytesseract.image_to_data(img, output_type='data.frame').values.tolist()
     boxes = list(filter(lambda x: isinstance(x[11], str) and x[11].strip(), words))
@@ -70,17 +146,37 @@ def select_numbers(img):
     print(f"Numbers obtained in {time.time() - s}s")
     print(numbers)
 
+    # rawNums = [x[0] for x in numbers]
+    # plt.scatter(rawNums, np.zeros(len(rawNums)))
+    # plt.show()
+
+    # Getting some variables ready for graph visualisation
     pos = {i: (numbers[i][1], numbers[i][2]) for i in range(len(numbers))}
-    labeldict = {i: numbers[i][0] for i in range(len(numbers))}
+
+    labeldict = {i: f"{i},{numbers[i][0]}" for i in range(len(numbers))}
+
+    # Creating the number graph
     G = nx.from_dict_of_lists(treeify(numbers))
-    nx.draw(G, labels = labeldict, with_labels=True, pos=pos)
+    nx.draw(G, labels=labeldict, pos=pos, with_labels=True, arrows=True)
     plt.show()
 
-    # numsets = generateNumsets(G)
+    # print(score([0, 1, 3, 5, 6]))
+
+    # Sets of numbers
+    numsets = generateNumsets(G)
+    scores = list(map(score, numsets))
+    ranking = np.argsort(scores)
+    print(numsets[ranking[0]])
+    # n = len(ranking)
+    # for i in range(n):
+    #     print(numsets[ranking[i]], score(numsets[ranking[i]]), numbersFromNumset(numsets[ranking[i]]))
+    # plt.show()
+    # print(numsets)
     # for path in numsets:
+    #     print(path)
     #     H = nx.from_edgelist([(path[i], path[i+1]) for i in range(len(path) - 1)])
     #     nx.draw(H, labels = labeldict, with_labels=True, pos=pos)
     #     plt.show()
 
 # print(pytesseract.image_to_data(load('numexample1.png')))
-print(select_numbers(load('images/numexample1.png')))
+print(select_numbers(load('images/mathtest2.png')))
